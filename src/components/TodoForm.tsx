664ca84547e398479b36c9bfc07d1e3a5753727d
David <depch47@gmail.com>
2022-05-01 13:48:06 -0500

    ADD---line-through decoration and color

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -53,7 +53,7 @@ const TodoForm: React.FC = () => {
         </div>
         <div className='md:w-2/3'>
           <textarea
-            className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-orange-500'
+            className='max-h-20 resize-none bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-orange-500'
             id='inline-full-name'
             onChange={(e) => setUserInput({ title, isDone, message: e.target.value })}
             value={message}
