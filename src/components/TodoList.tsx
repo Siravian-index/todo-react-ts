342545e5ac750e11a95465bb910b423cc674372c
David <depch47@gmail.com>
2022-05-02 19:07:24 -0500

    ADD remove functionality

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { updateTodo } from '../service/todoService'
+import { deleteTodo, updateTodo } from '../service/todoService'
 import { useTodoState } from '../stateManagement/ContextProvider'
 import { todo, todoAction } from '../stateManagement/reducer'
 import Todo from './Todo'
@@ -13,7 +13,10 @@ const TodoList: React.FC = () => {
   }
 
   const deleteOnClick = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, todo: todo) => {
-    dispatch({ type: todoAction.REMOVE, payload: todo })
+    const shouldBeDeleted: boolean = await deleteTodo(todo)
+    if (shouldBeDeleted) {
+      dispatch({ type: todoAction.REMOVE, payload: todo })
+    }
   }
   return (
     <div className='flex flex-col gap-4 mt-4 md:flex-row md:justify-evenly flex-wrap'>
