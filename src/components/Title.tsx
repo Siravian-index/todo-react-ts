bf67fa12a46f364630dbb4ae23d6e64e076c10fb
David <depch47@gmail.com>
2022-05-02 21:11:51 -0500

    Overengineering---Title component

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import { useTodoState } from '../stateManagement/ContextProvider'
-import { todo } from '../stateManagement/reducer'
 
 interface ITitleProps {
   setShowForm: React.Dispatch<React.SetStateAction<boolean>>
@@ -8,12 +7,12 @@ interface ITitleProps {
 
 const Title: React.FunctionComponent<ITitleProps> = ({ setShowForm }) => {
   const ZERO_TODOS = 0
+  const SINGULAR = 1
   const { state: todoList } = useTodoState()
   const [pendingTodos, setPendingTodos] = React.useState(0)
-  const title = 'Todo App'
+  const title = pendingTodos > SINGULAR ? 'Todos' : 'Todo'
   React.useEffect(() => {
-    const todoPending = todoList.filter((todo) => !todo.isDone)
-    setPendingTodos(todoPending.length)
+    setPendingTodos(todoList.reduce((c, t) => (!t.isDone ? ++c : c), 0))
   }, [todoList])
 
   return (
@@ -22,9 +21,8 @@ const Title: React.FunctionComponent<ITitleProps> = ({ setShowForm }) => {
         className='mt-2 text-4xl font-bold leading-normal mb-2 text-orange-500 hover:text-orange-400 hover:underline cursor-pointer'
         onClick={() => setShowForm((prev) => !prev)}
       >
-        {title}
+        {pendingTodos > ZERO_TODOS ? `${title} left: ${pendingTodos}` : `${title} App`}
       </h1>
-      {pendingTodos > ZERO_TODOS && <h2>Todos Left {pendingTodos}</h2>}
     </>
   )
 }
